f699cc0d08f65cf538abdb58f64535fc835692fb
Dominik Nakamura <dnaka91@gmail.com>
2021-04-06 22:51:17 +0900

    Use real favicon instead of emoji title

--- src/routes.rs
+++ src/routes.rs
@@ -27,6 +27,25 @@ pub(super) mod filters {
         warp::get().map(handlers::index)
     }
 
+    /// GET /favicon-{w}x{h}.png
+    pub(super) fn favicon() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+        favicon_32().or(favicon_16())
+    }
+
+    /// GET /favicon-32x32.png
+    fn favicon_32() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+        warp::get()
+            .and(warp::path!("favicon-32x32.png"))
+            .map(handlers::favicon_32)
+    }
+
+    /// GET /favicon-16x16.png
+    fn favicon_16() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+        warp::get()
+            .and(warp::path!("favicon-16x16.png"))
+            .map(handlers::favicon_16)
+    }
+
     /// GET /?category=ScienceAndTechnology&query=rust
     pub(super) fn search(
         client: Client,
@@ -42,13 +61,21 @@ pub(super) mod handlers {
     use std::convert::Infallible;
 
     use log::error;
-    use warp::Reply;
+    use warp::{http::Response, Reply};
 
     use super::{Client, SearchParams};
     use crate::templates;
 
     pub(super) fn index() -> impl Reply {
         templates::Index
+    }
+
+    pub(super) fn favicon_32() -> impl Reply {
+        Response::new(&include_bytes!("../assets/favicon-32x32.png")[..])
+    }
+
+    pub(super) fn favicon_16() -> impl Reply {
+        Response::new(&include_bytes!("../assets/favicon-16x16.png")[..])
     }
 
     pub(super) async fn search(
@@ -91,6 +118,7 @@ pub(super) mod handlers {
 
 pub fn build(client: Client) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     filters::search(client)
+        .or(filters::favicon())
         .or(filters::index())
         .with(warp::log("api"))
         .with(warp::compression::gzip())
