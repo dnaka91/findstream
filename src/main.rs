07cd5c5ad9cb45de48a1b6d49cb89688c99d1b49
Dominik Nakamura <dnaka91@gmail.com>
2022-02-09 22:48:31 +0900

    Go back to only bind to an IPv4 address

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,12 @@
 
 use std::{
     env,
-    future::Future,
-    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
+    net::{Ipv4Addr, SocketAddr},
     sync::Arc,
 };
 
 use anyhow::Result;
-use axum::{routing::IntoMakeService, Router, Server};
+use axum::Server;
 use tokio::sync::Mutex;
 use tokio_shutdown::Shutdown;
 use tracing::{info, Level};
@@ -27,17 +26,11 @@ mod settings;
 mod templates;
 mod twitch;
 
-const ADDRESS_V4: Ipv4Addr = if cfg!(debug_assertions) {
+const ADDRESS: Ipv4Addr = if cfg!(debug_assertions) {
     Ipv4Addr::LOCALHOST
 } else {
     Ipv4Addr::UNSPECIFIED
 };
-
-// const ADDRESS_V6: Ipv6Addr = if cfg!(debug_assertions) {
-//     Ipv6Addr::LOCALHOST
-// } else {
-//     Ipv6Addr::UNSPECIFIED
-// };
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -52,37 +45,19 @@ async fn main() -> Result<()> {
         .init();
 
     let settings = settings::load()?;
+    let shutdown = Shutdown::new()?;
     let client = Client::new(settings.client_id, settings.client_secret).await?;
     let client = Arc::new(Mutex::new(client));
 
-    let app = routes::build(client).into_make_service();
-
-    let shutdown = Shutdown::new()?;
-
-    let server_v4 = build_server(ADDRESS_V4.into(), &shutdown, app.clone())?;
-    // let server_v6 = build_server(ADDRESS_V6.into(), &shutdown, app)?;
-
-    let (res_v4,) = tokio::join!(tokio::spawn(server_v4));
-
-    res_v4??;
-    // res_v6??;
-
-    Ok(())
-}
-
-fn build_server(
-    addr: IpAddr,
-    shutdown: &Shutdown,
-    service: IntoMakeService<Router>,
-) -> Result<impl Future<Output = Result<()>>> {
-    let addr = SocketAddr::from((addr, 8080));
+    let addr = SocketAddr::from((ADDRESS, 8080));
 
     let server = Server::try_bind(&addr)?
-        .serve(service)
+        .serve(routes::build(client).into_make_service())
         .with_graceful_shutdown(shutdown.handle());
 
-    Ok(async move {
-        info!("Listening on {}", addr);
-        server.await.map_err(Into::into)
-    })
+    info!("Listening on {}", addr);
+
+    server.await?;
+
+    Ok(())
 }
